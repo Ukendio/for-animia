44fd1c9aa14ed40108a11d5f8533acb362d3ebce
Marcus <ukendio@gmail.com>
2022-03-02 03:07:10 +0100

    delete old remotes

--- src/shared/Remotes.ts
+++
@@ -1,6 +0,0 @@
-import Unit from "@rbxts/fabric/src/FabricLib/Fabric/Unit";
-import Net from "@rbxts/net";
-
-export const net_remotes = Net.Definitions.Create({
-	construct_unit: Net.Definitions.ServerToClientEvent<[keyof FabricUnits, Unit<keyof FabricUnits>["ref"]]>(),
-});

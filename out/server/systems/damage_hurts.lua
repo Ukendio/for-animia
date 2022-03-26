8f602a6ffa258513d790fdaf3aeef892a52f5506
Marcus <ukendio@gmail.com>
2022-03-26 01:10:16 +0100

    Delete out directory

--- out/server/systems/damage_hurts.lua
+++
@@ -1,6 +0,0 @@
--- Compiled with roblox-ts v1.3.3
-local function damage_hurts(world)
-end
-return {
-	damage_hurts = damage_hurts,
-}
--- out/shared/remotes.lua
+++
@@ -1,9 +0,0 @@
--- Compiled with roblox-ts v1.3.3
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
-local Net = TS.import(script, TS.getModule(script, "@rbxts", "net").out)
-return Net.Definitions.Create({
-	CreateFX = Net.Definitions.ClientToServerEvent(),
-	CreateFX2 = Net.Definitions.ClientToServerEvent(),
-	ReplicateFX = Net.Definitions.ServerToClientEvent(),
-	ReplicateFX2 = Net.Definitions.ServerToClientEvent(),
-})

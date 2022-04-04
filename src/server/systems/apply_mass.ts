63699beb563d692ae4cc7fc16de2dc8e65b125b5
Marcus <ukendio@gmail.com>
2022-04-04 19:24:00 +0200

    don't need Vec here

--- src/server/systems/apply_mass.ts
+++ src/server/systems/apply_mass.ts
@@ -1,11 +1,10 @@
-import { useEvent, World } from "@rbxts/matter";
-import { Vec } from "@rbxts/rust-classes";
+import { World } from "@rbxts/matter";
 import { Mass, Renderable, Target } from "shared/components";
 
 export function apply_mass(world: World): void {
 	for (const [, mass_record, { model }] of world.queryChanged(Mass, Renderable, Target)) {
 		if (mass_record.new) {
-			for (const v of Vec.fromPtr(model.GetDescendants()).generator()) {
+			for (const v of model.GetDescendants()) {
 				if (v.IsA("BasePart")) {
 					const { density, friction, elasticity } = mass_record.new;
 					v.CustomPhysicalProperties = new PhysicalProperties(density, friction, elasticity);

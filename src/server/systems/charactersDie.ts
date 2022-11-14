6d1f7a78237b8464809730d4ba05aba79cb9dcf9
Ukendio <ukendio@gmail.com>
2022-11-14 23:30:12 +0100

    Move characters to ServerStorage

--- src/server/systems/charactersDie.ts
+++ src/server/systems/charactersDie.ts
@@ -1,10 +1,20 @@
 import { World } from "@rbxts/matter";
-import { CombatStats, Renderable } from "shared/components";
+import { Widgets } from "@rbxts/plasma";
+import { ServerStorage } from "@rbxts/services";
+import { ServerState } from "server/index.server";
+import { Body, CombatStats, Renderable, Transform } from "shared/components";
 
-function charactersDie(world: World): void {
-	for (const [id, combatStats] of world.query(CombatStats, Renderable)) {
+function charactersDie(world: World, _: ServerState, ui: Widgets): void {
+	const clicked = ui.button("Die").clicked();
+	for (let [id, combatStats, renderable] of world.query(CombatStats, Body)) {
+		if (clicked) {
+			combatStats = combatStats.patch({ hp: 0 });
+		}
+		if (combatStats.state && combatStats.state === "Dead") continue;
+
 		if (combatStats.hp <= 0) {
-			world.despawn(id);
+			world.insert(id, combatStats.patch({ state: "Dead" }), Transform({ cf: renderable.model.GetPivot() }));
+			renderable.model.Parent = ServerStorage;
 		}
 	}
 }

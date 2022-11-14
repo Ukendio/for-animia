14c7e1d3b0acf0c8a79911162debefae1b7a36ff
Ukendio <ukendio@gmail.com>
2022-11-14 23:22:22 +0100

    Support multiple Model types for updateTransforms

--- src/shared/systems/updateTransforms.ts
+++ src/shared/systems/updateTransforms.ts
@@ -1,52 +1,54 @@
 import { World } from "@rbxts/matter";
 import { RunService } from "@rbxts/services";
-import { getModules } from "@rbxts/testez/src/TestBootstrap";
-import { Renderable, Transform } from "shared/components";
+import { Body, Renderable, Transform } from "shared/components";
+
+const MODEL_COMPONENTS = [Renderable, Body] as const;
 
 function updateTransforms(world: World): void {
 	for (const [id, transformRecord] of world.queryChanged(Transform)) {
 		if (!world.contains(id)) continue;
-
-		const renderable = world.get(id, Renderable);
-
-		if (!renderable) continue;
-
 		if (transformRecord.new && !transformRecord.new.doNotReconcile) {
-			renderable.model.PivotTo(transformRecord.new.cf);
+			const model = world.get(id, Renderable) ?? world.get(id, Body);
+
+			if (model) {
+				model.model.PivotTo(transformRecord.new.cf);
+			}
 		}
 	}
 
-	for (const [id, renderableRecord] of world.queryChanged(Renderable)) {
-		if (!world.contains(id)) {
-			continue;
+	for (const Model of MODEL_COMPONENTS) {
+		for (const [id, renderableRecord] of world.queryChanged(Model)) {
+			if (!world.contains(id)) {
+				continue;
+			}
+
+			const transform = world.get(id, Transform);
+
+			if (!transform) {
+				continue;
+			}
+
+			if (renderableRecord.new) {
+				renderableRecord.new.model.PivotTo(transform.cf);
+			}
 		}
 
-		const transform = world.get(id, Transform);
+		for (const [id, { model }, { cf }] of world.query(Model, Transform)) {
+			if (model.PrimaryPart?.Anchored) {
+				continue;
+			}
 
-		if (!transform) {
-			continue;
-		}
+			const existingCF = cf;
+			const currentCF = model.GetPivot();
 
-		if (renderableRecord.new) {
-			renderableRecord.new.model.PivotTo(transform.cf);
-		}
-	}
+			if (currentCF.Y < -400) {
+				world.despawn(id);
+				continue;
+			}
 
-	for (const [id, { model }, { cf }] of world.query(Renderable, Transform)) {
-		if (model.PrimaryPart?.Anchored) {
-			continue;
-		}
-
-		const existingCF = cf;
-		const currentCF = model.GetPivot();
-
-		if (currentCF.Y < -400) {
-			world.despawn(id);
-			continue;
-		}
-
-		if (currentCF !== existingCF) {
-			world.insert(id, Transform({ cf: currentCF, doNotReconcile: true }));
+			if (currentCF !== existingCF) {
+				world.insert(id, Transform({ cf: currentCF, doNotReconcile: true }));
+			}
 		}
 	}
 }
